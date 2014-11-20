0fdae404c7970d8d21e6ae95de086ad4d51a356f
proyectcharly <proyect.charly@gmail.com>
2014-11-19 20:14:51 -0600

    1.3
    portafolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+
 /***************** Waypoints ******************/
 
 $(document).ready(function() {

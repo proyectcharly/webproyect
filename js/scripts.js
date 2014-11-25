2d1bb09bb0ab088781d97c2c2907f6862805d7c0
proyectcharly <proyect.charly@gmail.com>
2014-11-24 21:58:03 -0600

    1.5
    depuracion

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+
 
 
 /***************** Waypoints ******************/

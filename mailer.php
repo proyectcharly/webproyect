de9c5391d3bf33f45a8cace988df848343fdd17d
proyectcharly <proyect.charly@gmail.com>
2014-12-09 15:47:43 -0600

    1.6
    form1.2

---
+++ mailer.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once("phpmailer/class.phpmailer.php");
+$mail = new PHPMailer();
+
+
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = "cpanel.possibleapp.com";
+$mail->Port = 465;
+$mail->Username = "contacto@proyect.agency";
+$mail->Password = "hola2014.%";
+
+$mail->SetFrom('contacto@proyect.agency', 'proyect.agency');
+$mail->Subject = "A Transactional Email From Web App";
+$mail->MsgHTML($body);
+$mail->AddAddress($address, $name);
+
+if($mail->Send()) {
+  echo "Message sent!";
+} else {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+}
+
+/* Set e-mail recipient */
+$myemail = "contacto@proyect.agency";
+
+/* Check all form inputs using check_input function */
+$name = check_input($_POST['inputName'], "Your Name");
+$email = check_input($_POST['inputEmail'], "Your E-mail Address");
+$subject = check_input($_POST['inputSubject'], "Message Subject");
+$message = check_input($_POST['inputMessage'], "Your Message");
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+show_error("Invalid e-mail address");
+}
+/* Let's prepare the message for the e-mail */
+
+$subject = "Someone has sent you a message";
+
+$message = "
+
+Someone has sent you a message using your contac form:
+
+Name: $name
+Email: $email
+Subject: $subject
+
+Message:
+$message
+
+";
+
+/* Send the message using mail() function */
+mail($myemail, $subject, $message);
+
+/* Redirect visitor to the thank you page */
+header('Location: gracias.html');
+exit();
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
+}
+return $data;
+}
+
+function show_error($myError)
+{
+?>
+<html>
+<body>
+
+<p>Please correct the following error:</p>
+<strong><?php echo $myError; ?></strong>
+<p>Hit the back button and try again</p>
+
+</body>
+</html>
+<?php
+exit();
+}
+?>

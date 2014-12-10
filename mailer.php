13aface80b1602d1e3793c58ce06443ea2b9674e
proyectcharly <proyect.charly@gmail.com>
2014-12-10 00:31:29 -0600

    1.9
    mailer bien

--- mailer.php
+++ mailer.php
@@ -1,43 +1,42 @@
 <?php
 
 require("phpmailer/class.phpmailer.php");
-
 $mail = new PHPMailer();
-$mail->SMTPDebug = 4;
+
+/*
 $mail->IsSMTP();
 $mail->SMTPAuth = true;
 $mail->Host = "cpanel.possibleapp.com";
-$mail->Port = 587;
+$mail->Port = 465;
 $mail->Username = "contacto@proyect.agency";
 $mail->Password = "hola2014.%";
+
 $mail->SetFrom('contacto@proyect.agency', 'proyect.agency');
 $mail->Subject = "A Transactional Email From Web App";
-$mail->AddAddress("contacto@proyect.agency", "proyect");
-$mail->IsHTML(true);
-$mail->body = "el mensaje que quieres que vaya en tu mail";
- 
+$mail->MsgHTML($body);
+$mail->AddAddress("contacto@proyect.agency, proyect");
 
 if($mail->Send()) {
   echo "Message sent!";
 } else {
   echo "Mailer Error: " . $mail->ErrorInfo;
 }
-
+/*
 /* Set e-mail recipient */
 $myemail = "contacto@proyect.agency";
 
-/* Check all form inputs using check_input function 
+/* Check all form inputs using check_input function */
 $name = check_input($_POST['inputName'], "Your Name");
 $email = check_input($_POST['inputEmail'], "Your E-mail Address");
 $subject = check_input($_POST['inputSubject'], "Message Subject");
 $message = check_input($_POST['inputMessage'], "Your Message");
 
-/* If e-mail is not valid show error message 
+/* If e-mail is not valid show error message */
 if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
 {
 show_error("Invalid e-mail address");
 }
-/* Let's prepare the message for the e-mail 
+/* Let's prepare the message for the e-mail */
 
 $subject = "Someone has sent you a message";
 
@@ -54,11 +53,12 @@ $message
 
 ";
 
-/* Send the message using mail() function 
+/* Send the message using mail() function */
 mail($myemail, $subject, $message);
 
 /* Redirect visitor to the thank you page */
-
+header('Location: gracias.html');
+exit();
 
 /* Functions we used */
 function check_input($data, $problem='')
